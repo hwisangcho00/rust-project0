833b770abb2c4e35c52a482a5b6a96c053d06233
Joseph Cho <hwisang@seas.upenn.edu>
2024-08-30 18:24:48 -0400

    sqrt and b_n

--- part1/src/lib.rs
+++ part1/src/lib.rs
@@ -2,8 +2,17 @@
 ///
 /// Using built-in square root functions is not allowed.
 pub fn sqrt(n: u32) -> u32 {
-    todo!();
-}
+  let ans = {
+    let mut ans = 1;
+    loop {
+      if ans * ans > n {
+        break ans - 1;
+      }
+      ans += 1;
+    }
+  };
+  ans
+} 
 
 /// Consumes a sorted list of integers and a query integer. Returns the index of the query integer
 ///
@@ -16,7 +25,23 @@ pub fn sqrt(n: u32) -> u32 {
 /// }
 /// ```
 pub fn binary_search(arr: &[i32], query: i32) -> Option<u32> {
-    todo!();
+    let mut lo = 0;
+    let mut hi = arr.len();
+
+    while lo < hi {
+      let mid = lo + (hi - lo) / 2;
+
+      if arr[mid] == query {
+        return Some(mid as u32)
+      } else if arr[mid] > query {
+        hi = mid
+      } else {
+        lo = mid + 1
+      }
+    }
+
+    return None
+
 }
 
 /// Consumes a list of numbers representing daily rainfall. The list may contain -999 signifying
